97f84ccec7818e0319965dc33d309bc7c4d10944
alejandrozavaletam <alejandro.zavaleta.m@gmail.com>
2022-11-02 12:50:18 -0600

    Scripts for versions scipy and deep learning

---
+++ Deep Learning/versions_deep.py
@@ -0,0 +1,9 @@
+# keras
+import keras
+print('keras: %s' % keras.__version__)
+# theano
+#import theano
+#print('theano: %s' % theano.__version__)
+# tensorflow
+import tensorflow
+print('tensorflow: %s' % tensorflow.__version__)
