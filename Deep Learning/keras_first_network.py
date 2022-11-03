539c4514798c641e1804719464032fbe6bc1dde1
alejandrozavaletam <alejandro.zavaleta.m@gmail.com>
2022-11-02 18:42:30 -0600

    Compile keras model function

--- Deep Learning/keras_first_network.py
+++ Deep Learning/keras_first_network.py
@@ -23,3 +23,17 @@ model.add(Dense(12, input_shape=(8,), activation='relu')) #First hidden layer wi
 model.add(Dense(8, activation='relu')) #Second hidden layer with 8 nodes and activation function of ReLu
 model.add(Dense(1, activation='sigmoid')) #One Output layer with the activation of sigmoid 
 ...
+#compile the keras model: specify properties required when training the network
+#training a network means "finding the best set of weights" to map inputs to outputs
+
+#specify the loss function to use to evaluate a set of weights, use cross entropy as the loss argument for binary classification problems
+# It is defined in Keras as "binary_crossentropy".
+
+#the optimizer used to search through different weights for the network, the efficient stochastic gradient descent algorithm “adam“
+#It is a popular version of gradient descent because it automatically tunes itself
+#This gradient descent algorithm gives good results in a wide range of problems
+
+#and any optional metrics you want to collect and report during training. For this case we´ll use "accuracy" 
+#To collect and report the classification accuracy defined "via the metrics" argument.
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+...
